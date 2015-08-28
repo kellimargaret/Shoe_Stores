ee2dcec44ab441e9a44cff1230d0ab7c0398838a
Kelli Sommerdyke <kelli.sommerdyke@gmai.com>
2015-08-28 12:58:44 -0700

    Add getAll method to Brand class.

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -47,24 +47,24 @@
             //Assert
             $this->assertEquals([$test_brand], $result);
         }
-        //
-        // function testGetAll()
-        // {
-        //     //Arrange
-        //     $brand_name = "Nike";
-        //     $test_brand = new Brand($brand_name);
-        //     $test_brand->save();
-        //
-        //     $another_brand = "Doc Martin";
-        //     $test_brand2 = new Brand($another_brand);
-        //     $test_brand2->save();
-        //
-        //     //Act
-        //     $result = Brand::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_brand, $test_brand2], $result);
-        // }
+        
+        function testGetAll()
+        {
+            //Arrange
+            $brand_name = "Nike";
+            $test_brand = new Brand($brand_name);
+            $test_brand->save();
+
+            $another_brand = "Doc Martin";
+            $test_brand2 = new Brand($another_brand);
+            $test_brand2->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$test_brand, $test_brand2], $result);
+        }
 
 
     }
